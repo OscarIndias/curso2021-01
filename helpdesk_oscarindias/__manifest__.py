75c8b8337f59b4bb1150c0358a2e37faf319d3bc
oscar indias <oscar.indias@factorlibre.com>
2021-04-13 16:19:48 +0000

    primer modulo

---
+++ helpdesk_oscarindias/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "helpdesk_oscarindias",
+    "summary": "Module summary",
+    "version": "11.0.1.0.0",
+    
+    "category": "base",
+    "website": "https://factorlibre.com/",
+    "author": "Oscar Indias",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    
+    "data": [
+        "security/ir.model.access.csv",
+        "views/helpdesk_menu.xml"
+    ],
+    
+}
