067765bcef809f17a212cc8603070e7a9202bce3
oscar indias <oscar.indias@factorlibre.com>
2021-04-14 16:39:29 +0000

    seguridad y vistas

--- helpdesk_oscarindias/__manifest__.py
+++ helpdesk_oscarindias/__manifest__.py
@@ -14,8 +14,10 @@
     ],
     
     "data": [
+        "security/helpdesk_security.xml",
         "security/ir.model.access.csv",
-        "views/helpdesk_menu.xml"
+        "views/helpdesk_menu.xml",
+        "views/helpdesk_view.xml"
     ],
     
 }
--- helpdesk_oscarindias/models/helpdesk_ticket.py
+++ helpdesk_oscarindias/models/helpdesk_ticket.py
@@ -4,6 +4,24 @@ class Helpdeskticket(models.Model):
     _name = "helpdesk.ticket"
     _description = "Ticket"
 
-    name = fields.Char(string = "name")
+    name = fields.Char(string = "name", required = True)
     description = fields.Text(string = "description")
     date = fields.Date(string = "fecha")
+    estado = fields.Selection(
+        [('nuevo', 'Nuevo'), 
+        ('asignado', 'Asignado'),
+        ('proceso', 'En proceso'),
+        ('pendiente','Pendiente'),
+        ('resuelto', 'Resuelto'),
+        ('cancelado', 'Cancelado')],
+        string= 'State',
+        default= 'nuevo'
+    )
+    time = fields.Float(sting = 'Time')
+    assigned = fields.Boolean(string = 'Assigned',
+     readonly=True)
+    date_limit = fields.Date(string = 'Date Limit')
+    action_corrective = fields.Html(string='Corrective Action',
+     help = 'Descrive corrective actions to do')
+    action_preventive = fields.Html(string = 'Preventive Action',
+     help = 'Descrive preventive actions to do')
