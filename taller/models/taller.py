faf544b786b7e09814f41cf35d412700b6c80f87
OscarIndias <82155832+OscarIndias@users.noreply.github.com>
2021-04-15 17:09:40 +0200

    Delete taller directory

--- taller/models/taller.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class taller(models.Model):
-    _name = "taller.taller"
-    _description = "taller"
-
-    nombre = fields.Char(string = "nombre")
-    fecha = fields.Date(string = "fecha")
-    descripcion = fields.Text(string = "descripcion")
