0977425aaee21e02b0ef6c75eccf6b73c1707e2e
oscar indias <oscar.indias@factorlibre.com>
2021-04-20 16:41:26 +0000

    clase 6

--- helpdesk_oscarindias/models/helpdesk_ticket.py
+++ helpdesk_oscarindias/models/helpdesk_ticket.py
@@ -1,8 +1,44 @@
-from odoo import models,fields
+from odoo import models,fields, api
+class helpdeskticketAction(models.Model):
+    _name = 'helpdesk.ticket.action'
+    _description = 'Action'
+
+    name = fields.Char()
+    date = fields.Date()
+    ticket_id = fields.Many2one(
+        comodel_name='helpdesk.ticket',
+        string='ticket')
+    
+class helpdeskTicketTag(models.Model):
+    _name = 'helpdesk.ticket.tag'
+    _description = 'tag'
+    name =fields.Char()
+    ticket_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket',
+        relation='helpdesk_ticket_tag_rel',
+        column1='tag_id',
+        column2='ticket_id',
+        string='tickets')
 
 class Helpdeskticket(models.Model):
     _name = "helpdesk.ticket"
     _description = "Ticket"
+    tag_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket.tag',
+        relation='helpdesk_ticket_tag_rel',
+        column1='ticket_id',
+        column2='tag_id',
+        string='tags')
+    
+    action_ids = fields.One2many(
+        comodel_name='helpdesk.ticket.action',
+        inverse_name='ticket_id',
+        string='actions')
+    
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='user_id')
+    
 
     name = fields.Char(string = "name", required = True)
     description = fields.Text(string = "description")
@@ -54,3 +90,45 @@ class Helpdeskticket(models.Model):
     def cancelado(self):
         self.ensure_one()
         self.state = 'cancelado'
+
+    @api.depends('user_id')
+    def _compute_assigned(self):
+        for record in self:
+            record.assigned = self.user_id and True or False
+
+    ticket_qty = fields.Integer(
+        string='ticket Qty',
+        compute='_compute_ticket_qty')
+
+    @api.depends('user_id')
+    def _compute_ticket_qty(self):
+        for record in self:
+            other_tickets = self.env['helpdesk.ticket'].search([('user_id','=', record.user_id.id)])
+            record.ticket_qty = len(other_tickets)
+    
+    tag_name = fields.Char(
+        string='Tag Name')
+
+    def create_tag(self):
+        self.ensure_one()
+        #opcion 1
+        self.write({
+            'tag_ids': [(0,0,{'name': self.tag_name})]
+        })
+        # #opcion 2
+        # tag = self.env['helpdesk.ticket.tag'].create({
+        #     'name':sel.tag_name
+        # })
+        # self.write({
+        #     'tag_ids': [(4,tag_id,0)]
+        # })
+        # #opcion3
+        # tag = self.env['helpdesk.ticket.tag'].create({
+        #     'name':sel.tag_name
+        # })
+        # self.write({
+        #     'tag_ids': [(6,0,tag_id)]
+        # })
+        self.tag_name = False
+    
+    
